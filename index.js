0fd210a7bd74b858731ae9e33053002c3b54a19f
Jamie Tregilgas <jaytrig@hotmail.co.uk>
2017-03-11 09:57:53 +0000

    Stuff yo

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var path    = require("path");
 
+app.use('/img',express.static(path.join(__dirname, '/images')));
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname+'/views/index.html'));
 })
